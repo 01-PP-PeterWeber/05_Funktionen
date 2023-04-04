a62b4e9d03393c1d4f2f06ea46ff0b51e39a5182
Peter Weber <peter-aalen@gmx.net>
2023-04-04 09:17:48 +0200

    Funktionsaufruf (call)

--- script_01.js
+++ script_01.js
@@ -3,8 +3,11 @@
 /***** Funktionen 01 *****/
 // 1. Kapselung von Codeblöcken
 
+// Funktionsaufruf (call)
+test();
 
-
+// Funktionsrumpf (body) | callee
+// Funktionsdeklaration
 function test()
 {
     console.log("Hallo Daniel!");
